688c9e63f433cd9493426417714a83058abf58de
Lion <lbita2305@gmail.com>
2025-01-23 13:03:45 +0300

    Хендлеры обработки сообщений

---
+++ module_13_/module_13_2.py
@@ -0,0 +1,31 @@
+#Хендлеры обработки сообщений
+import asyncio
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.utils import executor
+
+
+api = None
+with open('bot_api.txt', 'r', encoding='utf-8') as file:
+    for line in file:
+        api = str(line)
+
+
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_message(message):
+    print('Введите команду /start, чтобы начать общение.')
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
